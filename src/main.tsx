929491cac0fb5bc72f81021a1df50c60f36db880
stasfilippov <kron888999@gmail.com>
2024-05-26 23:31:15 +0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+
+import { App } from '@/App'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
