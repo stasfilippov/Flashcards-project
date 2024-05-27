227626bd92c0fcea2dc23508a3f1f12e66779690
VladZimin <vlad.zimin1@gmail.com>
2024-05-27 20:12:50 +0600

    Add Button component

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import s from './button.module.scss'
+
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  children: ReactNode
+  className?: string
+  fullWidth?: boolean
+  variant?: 'primary' | 'secondary'
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+
+  return (
+    <Component
+      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      {...rest}
+    />
+  )
+}
