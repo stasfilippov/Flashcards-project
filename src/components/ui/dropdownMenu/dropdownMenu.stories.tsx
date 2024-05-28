aa078cf3ecef0b9c96b0e9f62504f6148175c512
VladZimin <vlad.zimin1@gmail.com>
2024-05-28 23:23:02 +0600

    Add stories for DropdownMenu

---
+++ src/components/ui/dropdownMenu/dropdownMenu.stories.tsx
@@ -0,0 +1,84 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import {
+  Edit2Outline,
+  LogOutOutline,
+  PersonOutline,
+  PlayCircleOutline,
+  TrashOutline,
+} from '@/assets/icons/components'
+import { Typography } from '@/components/ui/typography'
+
+import s from './styleForStories.module.scss'
+
+import { DropdownItem } from './dropdownItem'
+import { DropdownMenu } from './dropdownMenu'
+import { DropdownSeparator } from './dropdownSeparator'
+
+const meta = {
+  component: DropdownMenu,
+  tags: ['autodocs'],
+  title: 'Components/DropdownMenu',
+} satisfies Meta<typeof DropdownMenu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+const user = {
+  email: 'j&johnson@gmail.com',
+  name: 'Ivan',
+  photo: { alt: 'User photo', src: 'src/assets/img/avatar.png' },
+}
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <DropdownItem>
+          <PlayCircleOutline width={16} />
+          <Typography variant={'caption'}>Learn</Typography>
+        </DropdownItem>
+        <DropdownSeparator />
+        <DropdownItem>
+          <Edit2Outline width={16} />
+          <Typography variant={'caption'}>Edit</Typography>
+        </DropdownItem>
+        <DropdownSeparator />
+        <DropdownItem>
+          <TrashOutline width={16} />
+          <Typography variant={'caption'}>Delete</Typography>
+        </DropdownItem>
+      </>
+    ),
+  },
+}
+
+export const WithUser: Story = {
+  args: {
+    children: (
+      <>
+        <DropdownItem>
+          <div className={s.userInfo}>
+            <img alt={user.photo.alt} className={s.photoBlock} src={user.photo.src} />
+            <div className={s.infoBlock}>
+              <Typography variant={'subtitle2'}>{user.name}</Typography>
+              <Typography className={s.email} variant={'caption'}>
+                {user.email}
+              </Typography>
+            </div>
+          </div>
+        </DropdownItem>
+        <DropdownSeparator />
+        <DropdownItem>
+          <PersonOutline width={16} />
+          <Typography variant={'caption'}>My Profile</Typography>
+        </DropdownItem>
+        <DropdownSeparator />
+        <DropdownItem>
+          <LogOutOutline width={16} />
+          <Typography variant={'caption'}>Sign Out</Typography>
+        </DropdownItem>
+      </>
+    ),
+    trigger: <img alt={user.photo.alt} src={user.photo.src} />,
+  },
+}
