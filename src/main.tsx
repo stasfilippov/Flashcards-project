a06a993681c7992d9ae4061a41d75579a386de64
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-05-29 19:07:24 +0200

    Added routing support and storybook addon for react-router
    Integrating react-router-dom into project main component and storybook. Added `BrowserRouter` component wrapping the main App component. Also, included `react-router-dom` and `storybook-addon-remix-react-router` into package dependencies. This would enable routing capabilities and better storybook interaction with routes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import { App } from '@/App'
 import { createRoot } from 'react-dom/client'
@@ -9,6 +10,8 @@ import '@fontsource/roboto/700.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
