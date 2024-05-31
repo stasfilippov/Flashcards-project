53140b905f403130fb637c248eb44ea5f04b0be4
VladZimin <vlad.zimin1@gmail.com>
2024-05-31 10:54:20 +0600

    Add Modal component

---
+++ src/components/ui/modal/modal.tsx
@@ -0,0 +1,48 @@
+import { ComponentPropsWithoutRef, ReactNode } from 'react'
+
+import { Close } from '@/assets/icons/components'
+import { Typography } from '@/components/ui'
+import * as Dialog from '@radix-ui/react-dialog'
+import clsx from 'clsx'
+
+import s from './modal.module.scss'
+type Props = {
+  children: ReactNode
+  className?: string
+  onClose?: () => void
+  open: boolean
+  title?: string
+  withCloseBtn?: boolean
+} & ComponentPropsWithoutRef<typeof Dialog.Content>
+
+export const Modal = (props: Props) => {
+  const { children, className, onClose, open, title, withCloseBtn = true } = props
+
+  const closeModalHandler = () => {
+    onClose?.()
+  }
+
+  return (
+    <Dialog.Root onOpenChange={closeModalHandler} open={open}>
+      <Dialog.Portal>
+        <Dialog.Overlay className={s.overlay}>
+          <Dialog.Content className={clsx(s.modal, className)}>
+            <div className={s.header}>
+              <Dialog.Title asChild>
+                <Typography variant={'h3'}>{title}</Typography>
+              </Dialog.Title>
+              {withCloseBtn && (
+                <Dialog.Close asChild className={s.closeIcon}>
+                  <Close width={24} />
+                </Dialog.Close>
+              )}
+            </div>
+            <div className={s.content}>
+              <Typography variant={'body1'}>{children}</Typography>
+            </div>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
