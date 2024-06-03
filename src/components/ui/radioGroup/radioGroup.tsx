517564b15f748ff1240f223db9251cb78ef4850d
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-06-03 14:02:48 +0200

    Update RadioGroup component props and stories
    Modified the Props type and adjusted options format for the RadioGroup component. Rather than just accepting an array of strings, options now accepts an array of custom objects with 'label' and 'value' fields. This change also required updates to component stories to reflect these changes, thus allowing a more structured and explicit way of defining options.

--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -1,30 +1,38 @@
 import { Typography } from '@/components/ui'
 import * as RadioGroupRadix from '@radix-ui/react-radio-group'
+import clsx from 'clsx'
 
 import s from './radioGroup.module.scss'
 
+type Options = {
+  label: string
+  value: string
+}
+
 type Props = {
-  options: string[]
+  options: Options[]
 } & RadioGroupRadix.RadioGroupProps
 
 export const RadioGroup = ({ children, className, defaultValue, options, ...rest }: Props) => {
   return (
-    <form className={className}>
-      <RadioGroupRadix.Root className={s.radioGroupRoot} defaultValue={defaultValue} {...rest}>
-        {options.map((o, i) => {
-          return (
-            <label className={s.label} key={`${o}-${i}`}>
-              <div className={s.radioGroupItemContainer}>
-                <RadioGroupRadix.Item className={s.radioGroupItem} value={o}>
-                  <RadioGroupRadix.Indicator className={s.radioGroupIndicator} />
-                </RadioGroupRadix.Item>
+    <RadioGroupRadix.Root
+      className={clsx(s.radioGroupRoot, className)}
+      defaultValue={defaultValue}
+      {...rest}
+    >
+      {options.map((o, i) => {
+        return (
+          <label className={s.label} key={`${o}-${i}`}>
+            <div className={s.radioGroupItemContainer}>
+              <RadioGroupRadix.Item className={s.radioGroupItem} value={o.value}>
+                <RadioGroupRadix.Indicator className={s.radioGroupIndicator} />
+              </RadioGroupRadix.Item>
 
-                <Typography variant={'body2'}>{o}</Typography>
-              </div>
-            </label>
-          )
-        })}
-      </RadioGroupRadix.Root>
-    </form>
+              <Typography variant={'body2'}>{o.label}</Typography>
+            </div>
+          </label>
+        )
+      })}
+    </RadioGroupRadix.Root>
   )
 }
