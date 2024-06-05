48db3382daee013d958e8a60d37674b1114484fc
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-06-05 22:11:32 +0200

    Add route constants and modify textField utils export
    Introduced route constants by creating a new routes.ts file in the constants directory. These constants aim to centralize definition of all the route paths in the application. Additionally, removed the unnecessary export statement from textField utils index.ts file.

---
+++ src/common/constants/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  base: '/',
+  checkEmail: 'check-email',
+  createNewPassword: 'create-new-password',
+  forgotPassword: 'forgot-password',
+  profile: 'profile',
+  signIn: 'sign-in',
+  signUp: 'sign-up',
+} as const
