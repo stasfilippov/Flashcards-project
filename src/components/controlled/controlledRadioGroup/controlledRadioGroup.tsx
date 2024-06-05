c7830dd105546546634f980e72c5e54fcaae78e9
VladZimin <vlad.zimin1@gmail.com>
2024-06-05 18:56:37 +0600

    Add ControlledRadioGroup component

---
+++ src/components/controlled/controlledRadioGroup/controlledRadioGroup.tsx
@@ -0,0 +1,32 @@
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+import { RadioGroup, RadioProps } from '@/components/ui/radioGroup/radioGroup'
+
+export type ControlledRadioProps<T extends FieldValues> = Omit<
+  RadioProps,
+  'disabled' | 'name' | 'onValueChange' | 'ref' | 'value'
+> &
+  UseControllerProps<T>
+
+export const ControlledRadioGroup = <T extends FieldValues>({
+  control,
+  defaultValue,
+  disabled,
+  name,
+  rules,
+  shouldUnregister,
+  ...rest
+}: ControlledRadioProps<T>) => {
+  const {
+    field: { onChange, ...field },
+  } = useController({
+    control,
+    defaultValue,
+    disabled,
+    name,
+    rules,
+    shouldUnregister,
+  })
+
+  return <RadioGroup {...rest} {...field} onValueChange={onChange} />
+}
--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -1,3 +1,5 @@
+import { ElementRef, forwardRef } from 'react'
+
 import { Typography } from '@/components/ui'
 import * as RadioGroupRadix from '@radix-ui/react-radio-group'
 import clsx from 'clsx'
@@ -9,30 +11,32 @@ type Options = {
   value: string
 }
 
-type Props = {
+export type RadioProps = {
   options: Options[]
 } & RadioGroupRadix.RadioGroupProps
 
-export const RadioGroup = ({ children, className, defaultValue, options, ...rest }: Props) => {
-  return (
-    <RadioGroupRadix.Root
-      className={clsx(s.radioGroupRoot, className)}
-      defaultValue={defaultValue}
-      {...rest}
-    >
-      {options.map((o, i) => {
-        return (
-          <label className={s.label} key={`${o}-${i}`}>
-            <div className={s.radioGroupItemContainer}>
-              <RadioGroupRadix.Item className={s.radioGroupItem} value={o.value}>
-                <RadioGroupRadix.Indicator className={s.radioGroupIndicator} />
-              </RadioGroupRadix.Item>
+export const RadioGroup = forwardRef<ElementRef<typeof RadioGroupRadix.Root>, RadioProps>(
+  ({ children, className, defaultValue, options, ...rest }) => {
+    return (
+      <RadioGroupRadix.Root
+        className={clsx(s.radioGroupRoot, className)}
+        defaultValue={defaultValue}
+        {...rest}
+      >
+        {options.map((o, i) => {
+          return (
+            <label className={s.label} key={`${o}-${i}`}>
+              <div className={s.radioGroupItemContainer}>
+                <RadioGroupRadix.Item className={s.radioGroupItem} value={o.value}>
+                  <RadioGroupRadix.Indicator className={s.radioGroupIndicator} />
+                </RadioGroupRadix.Item>
 
-              <Typography variant={'body2'}>{o.label}</Typography>
-            </div>
-          </label>
-        )
-      })}
-    </RadioGroupRadix.Root>
-  )
-}
+                <Typography variant={'body2'}>{o.label}</Typography>
+              </div>
+            </label>
+          )
+        })}
+      </RadioGroupRadix.Root>
+    )
+  }
+)
