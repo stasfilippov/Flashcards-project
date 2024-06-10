467f3edca7f509107d60a4ec286cf2b02971f861
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-06-10 09:33:55 +0200

    Add Redux dependencies and update route constants
    Updated the application dependencies to include @reduxjs/toolkit and react-redux packages. In addition, the routes constants file was updated to include slashes at the start of all paths for consistency. Also, added a new 'other' route to capture all other unspecified paths.

--- src/common/constants/routes.ts
+++ src/common/constants/routes.ts
@@ -1,9 +1,10 @@
 export const ROUTES = {
   base: '/',
-  checkEmail: 'check-email',
-  createNewPassword: 'create-new-password',
-  forgotPassword: 'forgot-password',
-  profile: 'profile',
-  signIn: 'sign-in',
-  signUp: 'sign-up',
+  checkEmail: '/check-email',
+  createNewPassword: '/create-new-password',
+  forgotPassword: '/forgot-password',
+  other: '*',
+  profile: '/profile',
+  signIn: '/sign-in',
+  signUp: '/sign-up',
 } as const
