65a478d54fa1a084cec524d8b1a9a01e49ae9d6b
VladZimin <vlad.zimin1@gmail.com>
2024-06-13 21:48:08 +0600

    add hook useDebounce

---
+++ src/common/hooks/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+export const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => clearTimeout(timeout)
+  }, [delay, value])
+
+  return debouncedValue
+}
