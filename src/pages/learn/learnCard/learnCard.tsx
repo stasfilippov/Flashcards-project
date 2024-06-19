1659eebcabbdfd8dfe8079f229c69b24a77c3397
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-06-19 19:30:33 +0200

    Add styling to LearnCard component and refactor file structure
    Implemented CSS module for the LearnCard component, enhancing its visual presentation. The relevant TypeScript file was also refactored, including the import path of module styles and updating its use. The use of classNames for styling is now more organized and dedicated to each component element.

--- src/pages/learn/learnCard/learnCard.tsx
+++ src/pages/learn/learnCard/learnCard.tsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 
-import { RateYourselfForm } from '@/components/forms/rateYourself/rateYourselfForm'
-import { Button, Card } from '@/components/ui'
-import { useLearnPage } from '@/pages/learn/hooks/useLearnPage'
+import { RateYourselfForm } from '@/components/forms'
+import { Button, Card, Typography } from '@/components/ui'
+import clsx from 'clsx'
+
+import s from './learnCard.module.scss'
+
+import { useLearnPage } from '../hooks/useLearnPage'
 
 export const LearnCard = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -15,23 +19,49 @@ export const LearnCard = () => {
     setIsOpen(false)
   }
 
+  const classNames = {
+    answer: clsx(s.answer),
+    answerImg: clsx(s.img, s.answerImg),
+    attempts: clsx(s.attempts),
+    card: clsx(s.card),
+    container: clsx(s.container),
+    question: clsx(s.question),
+    questionImg: clsx(s.img, s.questionImg),
+  }
+
   return (
-    <Card title={`Learn "${deck?.name}"`}>
-      Question: {currentCard?.question}
-      {currentCard?.questionImg && <img alt={'question'} src={currentCard.questionImg} />}
-      Number of attempts to answer the question: {currentCard?.shots}
-      {!isOpen && (
-        <Button fullWidth onClick={showAnswerHandler}>
-          Show Answer
-        </Button>
-      )}
-      {isOpen && (
-        <>
-          Answer: {currentCard?.answer}
-          {currentCard?.answerImg && <img alt={'answer'} src={currentCard.answerImg} />}
-          <RateYourselfForm onSubmit={nextQuestionClickHandler} />
-        </>
-      )}
-    </Card>
+    <div className={classNames.container}>
+      <Card className={classNames.card} title={`Learn "${deck?.name}"`}>
+        <Typography className={classNames.question}>
+          <Typography variant={'subtitle1'}>Question:&nbsp;</Typography>
+          {currentCard?.question}
+        </Typography>
+        {currentCard?.questionImg && (
+          <img alt={'question'} className={classNames.questionImg} src={currentCard.questionImg} />
+        )}
+        <Typography className={classNames.attempts}>
+          Number of attempts to answer the question:&nbsp;
+          <Typography variant={'subtitle2'}>{currentCard?.shots}</Typography>
+        </Typography>
+
+        {!isOpen && (
+          <Button fullWidth onClick={showAnswerHandler}>
+            Show Answer
+          </Button>
+        )}
+        {isOpen && (
+          <>
+            <Typography className={classNames.answer}>
+              <Typography variant={'subtitle1'}>Answer:&nbsp;</Typography>
+              {currentCard?.answer}
+            </Typography>
+            {currentCard?.answerImg && (
+              <img alt={'answer'} className={classNames.answerImg} src={currentCard.answerImg} />
+            )}
+            <RateYourselfForm onSubmit={nextQuestionClickHandler} />
+          </>
+        )}
+      </Card>
+    </div>
   )
 }
