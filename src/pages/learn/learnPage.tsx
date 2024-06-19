032878c255cbb90f0eb87b1548c70628f805ca97
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-06-19 19:30:15 +0200

    Add 'rate yourself' form and update learnPage component styling
    A new file for the 'rate yourself' form has been implemented and exported for use. Additionally, the learnPage.component has been updated to include new styling for a better user experience. The BackNavigation class has specifically received styling updates.

--- src/pages/learn/learnPage.tsx
+++ src/pages/learn/learnPage.tsx
@@ -1,12 +1,19 @@
 import { ROUTES } from '@/common/constants'
 import { Page } from '@/components/layout'
 import { BackNavigation } from '@/pages/cardsPage/components'
+import clsx from 'clsx'
+
+import s from './learnPage.module.scss'
 
 import { useLearnPage } from './hooks/useLearnPage'
 import { LearnCard } from './learnCard'
 
 export const LearnPage = () => {
   const { cardError, cardIsLoading, currentCard, deck, deckError, deckIsLoading } = useLearnPage()
+
+  const classNames = {
+    backNavigation: clsx(s.backNavigationLink),
+  }
 
   return (
     <Page>
@@ -16,7 +23,11 @@ export const LearnPage = () => {
         <h1>Loading...</h1>
       ) : currentCard && deck ? (
         <>
-          <BackNavigation pageName={deck.name} route={`${ROUTES.decks}/${deck.id}`} />
+          <BackNavigation
+            className={classNames.backNavigation}
+            pageName={deck.name}
+            route={`${ROUTES.decks}/${deck.id}`}
+          />
           <LearnCard />
         </>
       ) : null}
